6191a321b3b8a5928fa35cc98bf5b6ab71e9536a
EC2 Default User <ec2-user@ip-172-31-6-204.us-east-2.compute.internal>
2020-07-13 16:11:35 +0000

    deploy and build scripts

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
-#13.58.12.120
-aws elb deregister-instances-from-load-balancer --load-balancer-name WISPCatLB --instances i-0c60c800044d3c51c
+INSTANCE_ID=$(ec2-metadata -i | awk '{ print $2 }')
+aws elb deregister-instances-from-load-balancer --load-balancer-name WISPCatLB --instances $INSTANCE_ID
 cd ~/RFCoverageWebServer/
 git pull
 sudo ~/RFCoverageWebServer/build_and_run.sh
-aws elb register-instances-with-load-balancer --load-balancer-name WISPCatLB --instances i-0c60c800044d3c51c
-
-
-
-
-
-#52.14.179.116
-aws elb deregister-instances-from-load-balancer --load-balancer-name WISPCatLB --instances i-00c3c5c0f6f5eb42b
-cd ~/RFCoverageWebServer/
-git pull
-sudo ~/RFCoverageWebServer/build_and_run.sh
-aws elb register-instances-with-load-balancer --load-balancer-name WISPCatLB --instances i-00c3c5c0f6f5eb42b
+aws elb register-instances-with-load-balancer --load-balancer-name WISPCatLB --instances $INSTANCE_ID

df4d3ab46f4cc998e2373e53194fd2e82b6be499
EC2 Default User <ec2-user@ip-172-31-27-23.us-east-2.compute.internal>
2020-07-30 19:59:01 +0000

    add sleep during deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 INSTANCE_ID=$(ec2-metadata -i | awk '{ print $2 }')
 aws elb deregister-instances-from-load-balancer --load-balancer-name WISPCatLB --instances $INSTANCE_ID
+sleep 5
 sudo docker stop `sudo docker ps -q`
 cd ~/RFCoverageWebServer/
 git pull

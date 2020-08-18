5431e42cfb0b602fc03dffd635413ba5bef824df
Ubuntu <ubuntu@ip-172-31-23-212.us-east-2.compute.internal>
2020-08-18 16:11:02 +0000

    increase gunicorn request timeout to 180

--- webserver/run_django_server.sh
+++ webserver/run_django_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-DJANGO_SETTINGS_MODULE=webserver.settings_prod gunicorn webserver.wsgi --bind :80 -w 6 --limit-request-line 0
+DJANGO_SETTINGS_MODULE=webserver.settings_prod gunicorn webserver.wsgi --bind :80 -w 6 -t 180

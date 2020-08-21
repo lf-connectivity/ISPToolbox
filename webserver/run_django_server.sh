cf06ccf0909bc50ce56b0cc4e33c490f9abaf68e
EC2 Default User <ec2-user@ip-172-31-23-175.us-east-2.compute.internal>
2020-08-21 19:45:53 +0000

    [wisp] add static files to deploy

--- webserver/run_django_server.sh
+++ webserver/run_django_server.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+DJANGO_SETTINGS_MODULE=webserver.settings_prod python manage.py collectstatic
 DJANGO_SETTINGS_MODULE=webserver.settings_prod gunicorn webserver.wsgi --bind :80 -w 6 -t 180

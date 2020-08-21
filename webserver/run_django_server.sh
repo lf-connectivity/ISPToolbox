5836862f5f3e29c9ac36befa6766a23cc4b56a35
EC2 Default User <ec2-user@ip-172-31-23-175.us-east-2.compute.internal>
2020-08-21 19:49:50 +0000

    [wisp] revert collectstatic

--- webserver/run_django_server.sh
+++ webserver/run_django_server.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-DJANGO_SETTINGS_MODULE=webserver.settings_prod python manage.py collectstatic
 DJANGO_SETTINGS_MODULE=webserver.settings_prod gunicorn webserver.wsgi --bind :80 -w 6 -t 180

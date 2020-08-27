ee6c05d482a754f58e296b092bb38113adcb3ec9
Ubuntu <ubuntu@ip-172-31-23-212.us-east-2.compute.internal>
2020-08-27 20:41:53 +0000

    start adding tasks for tree detection

---
+++ webserver/IspToolboxApp/Tasks/MapboxImageTasks.py
@@ -0,0 +1,26 @@
+from PIL import Image
+import requests
+import io
+
+
+mapbox_access_token = "pk.eyJ1IjoiZmJtYXBzIiwiYSI6ImNqOGFmamkxdTBmbzUyd28xY3lybnEwamIifQ.oabgbuGc81ENlOJoPhv4OQ"
+def getTileImages(tiles, supersample=3, progressUpdate=None):
+    urlTemplate = 'https://api.tiles.mapbox.com/v4/mapbox.satellite/{}/{}/{}.png?access_token={}'
+    images = []
+    for tile in tiles:
+        # super sample
+        dst = Image.new('RGB', (img_dim * (2**supersample), img_dim * (2**supersample)))
+        for j in range(2**supersample):
+            for i in range(2**supersample):
+                x = tile[0] * (2**supersample) + i
+                y = tile[1] * (2**supersample) + j
+                url = urlTemplate.format(target_zoom + supersample,x,y,mapbox_access_token)
+                response = requests.get(url, stream=True)
+                response.raw.decode_content = True
+                image_data = io.BytesIO(response.raw.read())
+                image = Image.open(image_data)
+                dst.paste(image, (i * img_dim, j * img_dim))
+        images.append(dst)
+        if progressUpdate:
+            progressUpdate()
+    return images

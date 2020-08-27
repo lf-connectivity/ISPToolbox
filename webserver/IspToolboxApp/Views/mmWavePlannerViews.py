9b6cd3206d1b3534b5db14231eb009a596097ea8
Ubuntu <ubuntu@ip-172-31-23-212.us-east-2.compute.internal>
2020-08-27 20:45:19 +0000

    allow iframe for mmwave planner

--- webserver/IspToolboxApp/Views/mmWavePlannerViews.py
+++ webserver/IspToolboxApp/Views/mmWavePlannerViews.py
@@ -1,7 +1,10 @@
 from django.views import View
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.decorators.clickjacking import xframe_options_exempt
 
+@method_decorator(xframe_options_exempt, name='dispatch')
 class MMWavePlannerView(View):
     def get(self, request):
         return render(request, 'mmWave/planner.html')

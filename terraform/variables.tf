5fdd5594b0f48ce5e8c7effea3172ac28cc11988
Victor <victorlee@fb.com>
2020-09-11 11:47:34 -0700

    terraform setup

---
+++ terraform/variables.tf
@@ -0,0 +1,49 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "us-east-2"
+}
+
+variable "aws_account_id" {
+  description = "AWS account ID"
+  default =
+}
+
+variable "access_key" {
+  description = "AWS access_key"
+  default = ""
+}
+
+variable "secret_key" {
+  description = "AWS secret_key"
+  default = ""
+}
+
+variable "az_count" {
+  description = "Number of AZs to cover in a given AWS region"
+  default     = "2"
+}
+
+variable "app_image" {
+  description = "Docker image to run in the ECS cluster"
+  default     = "nginx:latest"
+}
+
+variable "app_port" {
+  description = "Port exposed by the docker image to redirect traffic to"
+  default     = 80
+}
+
+variable "app_count" {
+  description = "Number of docker containers to run"
+  default     = 2
+}
+
+variable "fargate_cpu" {
+  description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
+  default     = "256"
+}
+
+variable "fargate_memory" {
+  description = "Fargate instance memory to provision (in MiB)"
+  default     = "512"
+}

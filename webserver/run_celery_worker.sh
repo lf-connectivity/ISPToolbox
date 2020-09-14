87efc61c520b1320919ab4243b996b021b9d935b
Ubuntu <ubuntu@ip-172-31-23-212.us-east-2.compute.internal>
2020-09-14 23:42:53 +0000

    [wisp] cleanup timeouts, oneoffs

--- webserver/run_celery_worker.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-DJANGO_SETTINGS_MODULE=webserver.settings_prod celery worker -A webserver --detach
--- webserver/run_django_server.sh
+++ webserver/run_django_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-DJANGO_SETTINGS_MODULE=webserver.settings_prod gunicorn webserver.wsgi --bind :80 -w 6 -t 180
+DJANGO_SETTINGS_MODULE=webserver.settings_prod gunicorn webserver.wsgi --bind :80 -w 6

c5fb26c3d5b1cadd2431121bbf487ecb9691b2ae
Ubuntu <ubuntu@ip-172-31-23-212.us-east-2.compute.internal>
2020-09-15 01:10:01 +0000

    change ports for webserver

--- webserver/run_django_server.sh
+++ webserver/run_django_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-DJANGO_SETTINGS_MODULE=webserver.settings_prod gunicorn webserver.wsgi --bind :80 -w 6
+DJANGO_SETTINGS_MODULE=webserver.settings_prod gunicorn webserver.wsgi --bind :8020 -w 6

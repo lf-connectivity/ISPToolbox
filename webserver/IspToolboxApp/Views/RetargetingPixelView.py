1c3169ced9a81a32fa9ad2028d0d438d0db75854
Ubuntu <ubuntu@ip-172-31-23-212.us-east-2.compute.internal>
2020-09-16 19:19:04 +0000

    [wip] views for pixel retargeting

--- webserver/IspToolboxApp/Views/RetargetingPixelView.py
+++ webserver/IspToolboxApp/Views/RetargetingPixelView.py
@@ -1,20 +1,43 @@
 from django.views import View
 from django.http import JsonResponse
+from IspToolboxApp.models import MarketingAccount, MarketingAudience
+from django.contrib.gis.geos import GEOSGeometry
+import json
 
 class MarketingAccountView(View):
-    def get(self, request):
-        return JsonResponse({})
-
     def post(self, request):
-        return JsonResponse({})
+        resp = {'error' : None}
+        try:
+            request.GET.get('fbid', None)
+            request.GET.get('access_token', None)
+        except Exception as e:
+            resp['error'] = str(e)
+        return JsonResponse(resp)
 
 class MarketingAudienceView(View):
     def get(self, request):
-        return JsonResponse({})
+        resp = {'error' : None}
+        try:
+
+        except Exception as e:
+            resp['error'] = str(e)
+        return JsonResponse(resp)
 
     def post(self, request):
-        return JsonResponse({})
+        resp = {'error' : None}
+        try:
+            request.GET.get('include', None)
+            request.GET.get('exclude', None)
+
+        except Exception as e:
+            resp['error'] = str(e)
+        return JsonResponse(resp)
     
 class MarketingAudienceGeoPixelCheck(View):
     def get(self, request):
-        return JsonResponse({})
+        resp = {'error' : None}
+        try:
+
+        except Exception as e:
+            resp['error'] = str(e)
+        return JsonResponse(resp)

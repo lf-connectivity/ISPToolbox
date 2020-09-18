bbd368bd03b38892ba7d044d695fc01505d809e2
Ubuntu <ubuntu@ip-172-31-23-212.us-east-2.compute.internal>
2020-09-18 01:32:17 +0000

    add dns record resource to terraform

--- terraform/computation/main.tf
+++ terraform/computation/main.tf
@@ -137,6 +137,25 @@ resource "aws_ecr_repository" "main" {
 # }
 ### ======================================== S3 ========================================
 
+### ======================================== route53 ========================================
+data "aws_route53_zone" "fbctower_domain" {
+  name         = "fbctower.com."
+}
+
+resource "aws_route53_record" "test_domain_record" {
+  allow_overwrite = true
+  name            = "test.${data.aws_route53_zone.fbctower_domain.name}"
+  type            = "A"
+  zone_id         = data.aws_route53_zone.fbctower_domain.zone_id
+
+  alias {
+    name                   = aws_alb.main.dns_name
+    zone_id                = aws_alb.main.zone_id
+    evaluate_target_health = false
+  }
+}
+### ======================================== route53 ========================================
+
 
 ### ======================================== ALB ========================================
 resource "aws_alb" "main" {

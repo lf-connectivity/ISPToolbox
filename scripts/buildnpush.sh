d3116256e3e6dd75af36505f374f3b4c88f41810
alexychongfb <57922324+alexychongfb@users.noreply.github.com>
2020-11-13 14:24:22 -0800

    add flower url to nginx (#65)

--- scripts/buildnpush.sh
+++ scripts/buildnpush.sh
@@ -1,6 +1,8 @@
-# aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin 623762516657.dkr.ecr.us-west-1.amazonaws.com
+# aws ecr get-login-password --region us-west-1 | sudo docker login --username AWS --password-stdin 623762516657.dkr.ecr.us-west-1.amazonaws.com
 docker-compose build
 docker tag isptoolbox-django:latest 623762516657.dkr.ecr.us-west-1.amazonaws.com/isptoolbox-django:latest
 docker push 623762516657.dkr.ecr.us-west-1.amazonaws.com/isptoolbox-django:latest
 docker tag isptoolbox-nginx:latest 623762516657.dkr.ecr.us-west-1.amazonaws.com/isptoolbox-nginx:latest
 docker push 623762516657.dkr.ecr.us-west-1.amazonaws.com/isptoolbox-nginx:latest
+docker tag isptoolbox-flower-nginx:latest 623762516657.dkr.ecr.us-west-1.amazonaws.com/isptoolbox-flower-nginx:latest
+docker push 623762516657.dkr.ecr.us-west-1.amazonaws.com/isptoolbox-flower-nginx:latest

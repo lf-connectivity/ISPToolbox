d5f0e254fa3342b9f855d3cf25602be16aca3af9
Maira Ahmad <mairaahmad@fb.com>
2021-01-04 17:03:38 +0000

    quick bug fix with css

--- webserver/static/mmwave/src/LinkObstructions.ts
+++ webserver/static/mmwave/src/LinkObstructions.ts
@@ -2,7 +2,7 @@ export function updateObstructionsData(obstructions: Array<[number, number]>) {
     if(obstructions.length > 0)
     {
         $("#isp-toolbox-link-status").html('Failed&nbsp;').css('color','red');
-        $("#isptoolbox-link-status-obstructions").text(`${obstructions.length} Obstructions`);
+        $("#isptoolbox-link-status-obstructions").text(`(${obstructions.length} Obstructions)`);
     } else {
         $("#isp-toolbox-link-status").html('Passed&nbsp;').css('color','green');
         $("#isptoolbox-link-status-obstructions").text('(No Obstructions)');

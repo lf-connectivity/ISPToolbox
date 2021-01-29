a471c71eca913324389fbcab7b3fd1919e022e8a
Kevin Zheng <kevinz@fb.com>
2021-01-29 19:13:53 +0000

    Made block_id_area a bigint due to CSV import errors

--- scripts/BBN/sql/broadbandnow.sql
+++ scripts/BBN/sql/broadbandnow.sql
@@ -7,6 +7,6 @@ CREATE TABLE IF NOT EXISTS "broadbandnow" (
     "population" integer NOT NULL,
     "block_id_area" bigint NOT NULL,
     "terrestrial_25_3" integer NOT NULL,
-    "minprice_broadband_plan_terrestrial" dec(5,2),
+    "minprice_broadband_plan_terrestrial" dec(5,2)
 );
 COMMIT;

de6623f97d7265a0e458d0dedac8fa10b2df88a7
Alex Chong <achong@fb.com>
2021-01-29 19:25:28 +0000

    fix linter failing

--- webserver/mmwave/lidar_utils/LidarEngine.py
+++ webserver/mmwave/lidar_utils/LidarEngine.py
@@ -149,7 +149,7 @@ class LidarEngine:
         data = np.asarray(combined_profile)
         mask = np.isnan(data)
         if np.all(mask):
-            raise LidarEngineException("""LiDAR data not available in this region. 
+            raise LidarEngineException("""LiDAR data not available in this region.
         Please try at a later date as our data routinely gets updated.""")
         else:
             data[mask] = np.interp(np.flatnonzero(mask), np.flatnonzero(~mask), data[~mask])

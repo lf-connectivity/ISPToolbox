c0d1ebf04bd8f7a924036799c570b1cefbe39922
Kevin Zheng <kevinz@fb.com>
2021-01-29 17:39:33 +0000

    Changed block_id_area to be bigint after failure to import some CSV data into database.

--- scripts/BBN/sql/broadbandnow.sql
+++ scripts/BBN/sql/broadbandnow.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS "broadbandnow" (
     "tract_id" bigint NOT NULL,
     "zip_code" varchar(5) NOT NULL,
     "population" integer NOT NULL,
-    "block_id_area" integer NOT NULL,
+    "block_id_area" bigint NOT NULL,
     "terrestrial_25_3" integer NOT NULL,
-    "minprice_broadband_plan_terrestrial" dec(5,2)
+    "minprice_broadband_plan_terrestrial" dec(5,2),
 );
 COMMIT;

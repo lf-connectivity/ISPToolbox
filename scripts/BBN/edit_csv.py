668781175a4a73e40e5ab4f15b0317433501d9f4
Kevin Zheng <kevinz@fb.com>
2021-02-02 00:39:23 +0000

    Fixed tab issue with edit_csv.py

--- scripts/BBN/edit_csv.py
+++ scripts/BBN/edit_csv.py
@@ -15,54 +15,55 @@ Output: example.csv -> example_edited.csv
 import csv
 import sys
 
+
 def main():
-	filename, extension = sys.argv[1].rsplit('.', 2)
-	infile = sys.argv[1]
-	outfile = '%s_edited.%s' % (filename, extension)
+    filename, extension = sys.argv[1].rsplit('.', 2)
+    infile = sys.argv[1]
+    outfile = '%s_edited.%s' % (filename, extension)
 
-	block_ids = set()
-	key_collisions = 0
-	padded_block_ids = 0
-	rows = 0
-	rows_written = 0
+    block_ids = set()
+    key_collisions = 0
+    padded_block_ids = 0
+    rows = 0
+    rows_written = 0
 
-	with open(infile) as f_in, open(outfile, 'w') as f_out:
-		csv_in = csv.reader(f_in)
-		csv_out = csv.writer(f_out)
+    with open(infile) as f_in, open(outfile, 'w') as f_out:
+        csv_in = csv.reader(f_in)
+        csv_out = csv.writer(f_out)
 
-		for i, row in enumerate(csv_in):
-			# Shorten rows with unnecessary trailing columns.
-			newrow = row[0:7]
+        for i, row in enumerate(csv_in):
+            # Shorten rows with unnecessary trailing columns.
+            newrow = row[0:7]
 
-			# Don't do anything to header
-			if i == 0:
-				csv_out.writerow(newrow)
-			else:
+            # Don't do anything to header
+            if i == 0:
+                csv_out.writerow(newrow)
+            else:
 
-				# Left pad block IDs.
-				padded_block_id = newrow[0].rjust(15, '0')
-				if newrow[0] != padded_block_id:
-					padded_block_ids += 1
-					newrow[0] = padded_block_id
+                # Left pad block IDs.
+                padded_block_id = newrow[0].rjust(15, '0')
+                if newrow[0] != padded_block_id:
+                    padded_block_ids += 1
+                    newrow[0] = padded_block_id
 
-				block_id = newrow[0]
+                block_id = newrow[0]
 
-				# Sanitize incomplete rows
-				if all(newrow):
-					if block_id in block_ids:
-						key_collisions += 1
-					else:
-						block_ids.add(block_id)
-						csv_out.writerow(newrow)
-						rows_written += 1
+                # Sanitize incomplete rows
+                if all(newrow):
+                    if block_id in block_ids:
+                        key_collisions += 1
+                    else:
+                        block_ids.add(block_id)
+                        csv_out.writerow(newrow)
+                        rows_written += 1
 
-				rows += 1
-	
-	print('%d key collisions found' % (key_collisions,))
-	print('%d block_ids padded' % (padded_block_ids))
-	print('%d rows processed' % (rows,))
-	print('%d rows written' % (rows_written,))
+                rows += 1
+    
+    print('%d key collisions found' % (key_collisions,))
+    print('%d block_ids padded' % (padded_block_ids))
+    print('%d rows processed' % (rows,))
+    print('%d rows written' % (rows_written,))
 
 if __name__ == '__main__':
-	main() 
-	
+    main() 
+    

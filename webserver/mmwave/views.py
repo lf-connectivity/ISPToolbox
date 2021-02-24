c6e5c6ba2af3a33441424d9f17613abc097d76f5
Alex Chong <achong@fb.com>
2021-02-24 00:04:03 +0000

    fix issue with sunflower sample being overwritten between requests

--- webserver/mmwave/views.py
+++ webserver/mmwave/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
 from django.views import View
-from django.views.decorators.cache import cache_control
-
 from django.utils.decorators import method_decorator
 from django.views.decorators.clickjacking import xframe_options_exempt
 from geopy.distance import distance as geopy_distance
 import geopy.point as geopy_pt
 import mmwave.lidar_utils.sample_links as samples
-
+import copy
 import uuid
 
 
@@ -17,7 +15,6 @@ class LOSCheckDemo(View):
     Demo view for LOS check, allows iframing, limited functionality, renders suggestion
     to sign up and unlock full version
     """
-    @cache_control(private=True)
     def get(self, request, network_id=None):
         fbid = int(request.GET.get('id', 0))
         networkID = uuid.uuid4()
@@ -25,8 +22,9 @@ class LOSCheckDemo(View):
         lon = request.GET.get('lon', None)
         units = request.GET.get('units', 'US')
 
-        tx = samples.sunflower['tx']
-        rx = samples.sunflower['rx']
+        # Make a deep copy of the sample points to avoid changing the samples for other requests
+        tx = copy.deepcopy(samples.sunflower['tx'])
+        rx = copy.deepcopy(samples.sunflower['rx'])
 
         if lat is not None and lon is not None:
             middle = geopy_pt.Point(float(lat), float(lon))
@@ -42,8 +40,6 @@ class LOSCheckDemo(View):
             'fbid': fbid,
             'networkID': networkID,
             'units': units,
-            'lat': lat,
-            'lon': lon,
             'demo': True
         }
         return render(request, 'mmwave/index.html', context)

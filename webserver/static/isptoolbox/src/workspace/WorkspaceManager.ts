f2da5497a4ed4b7dae7596e6da90c1303d386452
Kevin Zheng <kevinz@fb.com>
2021-04-09 18:29:34 +0000

    Added null/undefined check for if a user is not logged in and has no initial features.

--- webserver/static/isptoolbox/src/workspace/WorkspaceManager.ts
+++ webserver/static/isptoolbox/src/workspace/WorkspaceManager.ts
@@ -177,43 +177,45 @@ export class WorkspaceManager {
         // Initialize features
         this.features = {};
 
-        // Add APs and CPEs
-        const nonLinks = initialFeatures?.features.filter((feature: any) => {
-            return feature.properties.feature_type !== undefined && 
-                   (feature.properties.feature_type === WorkspaceFeatureTypes.AP ||
-                    feature.properties.feature_type === WorkspaceFeatureTypes.CPE);
-        });
+        // Add Workspace features from user, if they exist.
+        if (initialFeatures) {
+            const nonLinks = initialFeatures?.features.filter((feature: any) => {
+                return feature.properties.feature_type !== undefined && 
+                       (feature.properties.feature_type === WorkspaceFeatureTypes.AP ||
+                        feature.properties.feature_type === WorkspaceFeatureTypes.CPE);
+            });
+            
+            nonLinks.forEach((feature: any) => {
+                let feature_type = feature.properties.feature_type;
+                let workspaceFeature = undefined;
         
-        nonLinks.forEach((feature: any) => {
-            let feature_type = feature.properties.feature_type;
-            let workspaceFeature = undefined;
+                // Add all the APs and CPEs
+                if (feature_type === WorkspaceFeatureTypes.AP) {
+                    feature.properties.radius = feature.properties.max_radius;
+                    feature.properties.center = feature.geometry.coordinates;
+                    workspaceFeature = new AccessPoint(this.draw, feature);
+                }
+                else {
+                    workspaceFeature = new CPE(this.draw, feature);
+                }
+                this.features[workspaceFeature.workspaceId] = workspaceFeature;
+            });
     
-            // Add all the APs and CPEs
-            if (feature_type === WorkspaceFeatureTypes.AP) {
-                feature.properties.radius = feature.properties.max_radius;
-                feature.properties.center = feature.geometry.coordinates;
-                workspaceFeature = new AccessPoint(this.draw, feature);
-            }
-            else {
-                workspaceFeature = new CPE(this.draw, feature);
-            }
-            this.features[workspaceFeature.workspaceId] = workspaceFeature;
-        });
-
-        // Add links
-        const links = initialFeatures?.features.filter((feature: any) => {
-            return feature.properties.feature_type !== undefined && 
-                   feature.properties.feature_type === WorkspaceFeatureTypes.AP_CPE_LINK;
-        });
-        
-        links.forEach((feature: any) => {
-            let apWorkspaceId = feature.properties.ap;
-            let cpeWorkspaceId = feature.properties.cpe;
-            let ap = this.features[apWorkspaceId] as AccessPoint;
-            let cpe = this.features[cpeWorkspaceId] as CPE;
-            let workspaceFeature = new APToCPELink(this.draw, feature, ap, cpe);
-            this.features[workspaceFeature.workspaceId] = workspaceFeature;
-        });
+            // Add links
+            const links = initialFeatures?.features.filter((feature: any) => {
+                return feature.properties.feature_type !== undefined && 
+                       feature.properties.feature_type === WorkspaceFeatureTypes.AP_CPE_LINK;
+            });
+            
+            links.forEach((feature: any) => {
+                let apWorkspaceId = feature.properties.ap;
+                let cpeWorkspaceId = feature.properties.cpe;
+                let ap = this.features[apWorkspaceId] as AccessPoint;
+                let cpe = this.features[cpeWorkspaceId] as CPE;
+                let workspaceFeature = new APToCPELink(this.draw, feature, ap, cpe);
+                this.features[workspaceFeature.workspaceId] = workspaceFeature;
+            });
+        }
 
         // Initialize Constructors
         this.map.on('draw.create', this.saveFeatures.bind(this));

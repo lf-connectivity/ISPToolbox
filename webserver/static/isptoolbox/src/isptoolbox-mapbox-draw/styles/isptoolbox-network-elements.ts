7051778d430293277e86debf8bdad6c62727eb7e
Alex Chong <achong@fb.com>
2021-05-04 21:15:22 +0000

    add cross origin anonymous to cors svg

--- webserver/static/isptoolbox/src/isptoolbox-mapbox-draw/styles/isptoolbox-network-elements.ts
+++ webserver/static/isptoolbox/src/isptoolbox-mapbox-draw/styles/isptoolbox-network-elements.ts
@@ -3,11 +3,13 @@ import ap_icon from './ap-icon.svg';
 
 
 export function load_custom_icons(map: mapboxgl.Map) {
-    let img_cpe = new Image(20,20)
+    let img_cpe = new Image(20,20);
+    img_cpe.crossOrigin = "Anonymous";
     img_cpe.onload = ()=>map.addImage('cpe-isptoolbox', img_cpe);
     img_cpe.src = cpe_icon;
 
-    let img_ap = new Image(20,20)
+    let img_ap = new Image(20,20);
+    img_ap.crossOrigin = "Anonymous";
     img_ap.onload = ()=>map.addImage('ap-isptoolbox', img_ap);
     img_ap.src = ap_icon;
 }

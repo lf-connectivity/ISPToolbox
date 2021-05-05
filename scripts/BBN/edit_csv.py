6dc01e02394af7b9f86399f75e5e0fb3e8350cf6
Kevin Zheng <kevinz@fb.com>
2021-05-05 22:59:25 +0000

    Enhancements to the BBN CSV preprocessing script.

--- scripts/BBN/edit_csv.py
+++ scripts/BBN/edit_csv.py
@@ -7,27 +7,27 @@ Script to preprocess CSV files from BroadbandNow data. The script does three thi
 
 This is to make importing data easier.
 
-Usage: edit_csv.py <csv file>
+Usage: edit_csv.py <csv files>
 Output: example.csv -> example_edited.csv
 """
 
 
 import csv
 import sys
+import glob
 
+OUTFILE = 'bbn.csv'
+BLOCK_IDS = set()
 
-def main():
-    filename, extension = sys.argv[1].rsplit('.', 2)
-    infile = sys.argv[1]
-    outfile = '%s_edited.%s' % (filename, extension)
+def process_file(filename):
+    infile = filename
 
-    block_ids = set()
     key_collisions = 0
     padded_block_ids = 0
     rows = 0
     rows_written = 0
 
-    with open(infile) as f_in, open(outfile, 'w') as f_out:
+    with open(infile) as f_in, open(OUTFILE, 'a') as f_out:
         csv_in = csv.reader(f_in)
         csv_out = csv.writer(f_out)
 
@@ -35,10 +35,8 @@ def main():
             # Shorten rows with unnecessary trailing columns.
             newrow = row[0:7]
 
-            # Don't do anything to header
-            if i == 0:
-                csv_out.writerow(newrow)
-            else:
+            # Skip header.
+            if i != 0:
 
                 # Left pad block IDs.
                 padded_block_id = newrow[0].rjust(15, '0')
@@ -50,19 +48,30 @@ def main():
 
                 # Sanitize incomplete rows
                 if all(newrow):
-                    if block_id in block_ids:
+                    if block_id in BLOCK_IDS:
                         key_collisions += 1
                     else:
-                        block_ids.add(block_id)
+                        BLOCK_IDS.add(block_id)
                         csv_out.writerow(newrow)
                         rows_written += 1
 
                 rows += 1
 
-    print('%d key collisions found' % (key_collisions,))
-    print('%d block_ids padded' % (padded_block_ids))
-    print('%d rows processed' % (rows,))
-    print('%d rows written' % (rows_written,))
+    print(f'Finished processing {filename}')
+    print(f'{key_collisions} key collisions found')
+    print(f'{padded_block_ids} block_ids padded')
+    print(f'{rows} rows processed')
+    print(f'{rows_written} rows written\n\n')
+
+def main():
+    # Truncate output file
+    f = open(OUTFILE, 'w')
+    f.close()
+
+    # Get all files from sys argv
+    files = sum([glob.glob(pattern) for pattern in sys.argv[1:]], [])
+    for file in files:
+        process_file(file)
 
 
 if __name__ == '__main__':

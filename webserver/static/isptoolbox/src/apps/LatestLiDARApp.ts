e9259616d74ec5feecc083a3f59d10fc153da102
Alex Chong <achong@fb.com>
2021-06-16 21:05:37 +0000

    clean up nav bar for demo views, add link back to fb.com

--- webserver/static/isptoolbox/src/apps/LatestLiDARApp.ts
+++ webserver/static/isptoolbox/src/apps/LatestLiDARApp.ts
@@ -8,20 +8,20 @@ const SVG_ARROW = `<svg width="25" height="26" viewBox="0 0 25 26" fill="none" x
 <path d="M5.94922 19.9369L18.4658 7.42039M9.27442 6.71337L18.9371 6.94904L19.1728 16.6117" stroke="white" stroke-width="1.5"/>
 </svg>`;
 class NewGISDataLayer {
-    popup : mapboxgl.Popup;
-    popupCTA: mapboxgl.Popup;
+    popup: mapboxgl.Popup;
+    popupFreeze: boolean = false;
 
-    constructor(private map: mapboxgl.Map, private month: string, private year: string){
+    constructor(private map: mapboxgl.Map, private month: string, private year: string) {
         this.popup = new window.mapboxgl.Popup({
             closeButton: false,
             closeOnClick: false,
-            className: "lidar-availability-popup"
+            className: "lidar-availability-popup",
+
         });
-        this.popupCTA = new window.mapboxgl.Popup({
-            closeButton: false,
-            closeOnClick: true,
-            className: "lidar-availability-popup"
-        });
+        this.popup.on('close', (e: any) => {
+            this.popupFreeze = false;
+        })
+
         map.addSource(NEW_GIS_DATA_SOURCE, {
             type: 'geojson',
             data: { type: 'FeatureCollection', 'features': [] }
@@ -37,46 +37,62 @@ class NewGISDataLayer {
                 "fill-opacity": 0.5,
             }
         });
-             
+
         this.map.on('mouseenter', NEW_GIS_DATA_LAYER, (e: any) => {
             // Change the cursor style as a UI indicator.
             this.map.getCanvas().style.cursor = 'pointer';
-            var description = `Newly Added Data for ${this.month}/${this.year}`
-            // Populate the popup and set its coordinates
-            // based on the feature found.
-            this.popup.setLngLat(e.lngLat).setHTML(description).addTo(this.map);
+            var description = this.createPopupBody(e);
+            this.popup.setHTML(description).addTo(this.map);
+            if (!this.popupFreeze) {
+                this.popup.setLngLat(e.lngLat);
+            }
         });
 
-        this.map.on('mousemove', NEW_GIS_DATA_LAYER, (e : any) => {
-            this.popup.setLngLat(e.lngLat);
+        this.map.on('mousemove', NEW_GIS_DATA_LAYER, (e: any) => {
+            if (!this.popupFreeze) {
+                this.popup.setLngLat(e.lngLat);
+            }
         });
-                
-        this.map.on('mouseleave', NEW_GIS_DATA_LAYER, (e : any)=> {
+
+        this.map.on('mouseleave', NEW_GIS_DATA_LAYER, (e: any) => {
             this.map.getCanvas().style.cursor = '';
-            this.popup.remove();
+            if (!this.popupFreeze) {
+                this.popup.remove();
+            }
         });
 
         this.map.on('click', NEW_GIS_DATA_LAYER, (e: any) => {
-            const params = new URLSearchParams({
-                lat: e.lngLat.lat,
-                lon: e.lngLat.lng,
-            });
-            var description = `<a target="_parent" href="/demo/los-check/?${params.toString()}">Explore Region in LiDAR LOS Tool ${SVG_ARROW}</a>`
-            this.popupCTA.setLngLat(e.lngLat).setHTML(description).addTo(this.map);
+            var description = this.createPopupBody(e);
+            this.popup.setHTML(description);
+            this.popupFreeze = true;
+            this.popup.setLngLat(e.lngLat);
         });
+
         const geojson_filename = `${year}-${month}-01-latest-added-pt-clouds.geojson`;
-        const url = GIS_LAYER_PATH  + geojson_filename;
+        const url = GIS_LAYER_PATH + geojson_filename;
         $.getJSON(url, this.ajaxcallback.bind(this));
     }
 
-    ajaxcallback(data: any, status: string){
+    createPopupBody(event: any) {
+        const params = new URLSearchParams({
+            lat: event.lngLat.lat,
+            lon: event.lngLat.lng,
+        });
+        var description = `<a target="_parent" href="https://www.facebook.com/isptoolbox/line-of-sight-check/?${params.toString()}">Explore Region in LiDAR LOS Tool ${SVG_ARROW}</a>`
+        // var description = `Newly Added Data for ${this.month}/${this.year}`
+        // Populate the popup and set its coordinates
+        // based on the feature found.
+        return description;
+    }
+
+    ajaxcallback(data: any, status: string) {
         if (status === 'success') {
             const source = this.map.getSource(NEW_GIS_DATA_SOURCE);
             if (source && source.type === 'geojson') {
                 source.setData(data);
             }
         }
-    };  
+    };
 
 
 }

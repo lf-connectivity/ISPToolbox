42aebbd0a1ad228dc83d474f2b417c2d22a60643
Justin Derwee-Church <derwee@fb.com>
2021-06-16 18:39:20 +0000

    Complete websocket impl

--- webserver/static/isptoolbox/src/MarketEvaluatorPage.ts
+++ webserver/static/isptoolbox/src/MarketEvaluatorPage.ts
@@ -1,7 +1,22 @@
 // Create new mapbox Map
 import * as MapboxGL from "mapbox-gl";
 import MapboxDraw from "@mapbox/mapbox-gl-draw";
-import { MarketEvaluatorSidebarManager } from './organisms/MarketEvaluatorSidebarManager';
+import MarketEvaluatorWS, {
+    MarketEvaluatorWSResponse,
+    RDOFGeojsonResponse,
+    ZipGeojsonResponse,
+    CountyGeojsonResponse,
+    CensusBlockGeojsonResponse,
+    ViewshedGeojsonResponse,
+    MedianSpeedResponse,
+    ServiceProvidersResponse,
+    BroadbandNowResponse,
+    BuildingOverlaysResponse,
+    MedianIncomeResponse,
+    UUID
+} from "./MarketEvaluatorWS";
+//@ts-ignore
+import styles from "@mapbox/mapbox-gl-draw/src/lib/theme";
 
 import { LinkMode, OverrideDirect, OverrideSimple, APDrawMode, OverrideDrawPolygon } from './isptoolbox-mapbox-draw/index';
 import { ISPToolboxAbstractAppPage } from "./ISPToolboxAbstractAppPage";
@@ -9,20 +24,88 @@ import { ISPToolboxAbstractAppPage } from "./ISPToolboxAbstractAppPage";
 export class MarketEvaluatorPage extends ISPToolboxAbstractAppPage {
     map: MapboxGL.Map;
     draw: MapboxDraw;
+    marketEvalWS: MarketEvaluatorWS;
+    currentRequestUUID: UUID;
 
     constructor() {
-        super({
-            draw_link: LinkMode(),
-            simple_select: OverrideSimple(),
-            direct_select: OverrideDirect(),
-            draw_ap: APDrawMode(),
-            draw_polygon: OverrideDrawPolygon()
+        let initial_map_center = { 'lon': 0, 'lat': 0 };
+        let initial_zoom = 17;
+
+        try {
+            // @ts-ignore
+            initial_map_center = window.ISPTOOLBOX_SESSION_INFO.initialMapCenter.coordinates;
+            // @ts-ignore
+            initial_zoom = window.ISPTOOLBOX_SESSION_INFO.initialMapZoom;
+        } catch (err) { }
+
+        this.map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/satellite-streets-v11', // stylesheet location
+            center: initial_map_center, // starting position [lng, lat]
+            zoom: initial_zoom, // starting zoom
         });
 
-        MarketEvaluatorSidebarManager.getInstance().initializePopovers();
+        this.map.on('load', () => {
+        });
+
+        this.currentRequestUUID = '';
+        this.marketEvalWS = new MarketEvaluatorWS([this.ws_message_handler.bind(this)]);
     }
 
-    onMapLoad() {
-        // stuff might go here later
+    ws_message_handler(response: MarketEvaluatorWSResponse): void {
+        // For now, only one active request at a time
+        if (response.uuid !== this.currentRequestUUID) {
+            return;
+        }
+        switch (response.type) {
+            case 'building.overlays':
+                const buildings: BuildingOverlaysResponse = response.value as BuildingOverlaysResponse;
+                // TODO: Handle building overlays
+                break;
+            case 'median.income':
+                const medianIncome: MedianIncomeResponse = response.value as MedianIncomeResponse;
+                // TODO: Handle median income
+                break;
+            case 'service.providers':
+                const serviceProviders: ServiceProvidersResponse = response.value as ServiceProvidersResponse;
+                // TODO: Handle service providers
+                break;
+            case 'broadband.now':
+                const broadBandNow: BroadbandNowResponse = response.value as BroadbandNowResponse;
+                // TODO: Handle broadband now
+                break;
+            case 'median.speeds':
+                const medianSpeeds: MedianSpeedResponse = response.value as MedianSpeedResponse;
+                // TODO: Handle median speeds
+                break;
+            case 'polygon.area':
+                const polygonArea: number = parseFloat(response.value as string);
+                // TODO: Handle polygon area
+                break;
+            case 'grant.geog':
+                const grantGeog: RDOFGeojsonResponse = response.value as RDOFGeojsonResponse;
+                // TODO: Handle grant geography
+                break;
+            case 'zip.geog':
+                const zipGeog: ZipGeojsonResponse = response.value as ZipGeojsonResponse;
+                // TODO: Handle zip geography
+                break;
+            case 'county.geog':
+                const countyGeog: CountyGeojsonResponse = response.value as CountyGeojsonResponse;
+                // TODO: Handle county geography
+                break;
+            case 'censusblock.geog':
+                const censusBlockGeog: CensusBlockGeojsonResponse = response.value as CensusBlockGeojsonResponse;
+                // TODO: Handle census block geography
+                break;
+            case 'tower.viewshed':
+                const viewshed: ViewshedGeojsonResponse = response.value as ViewshedGeojsonResponse;
+                // TODO: Handle viewshed
+                break;
+            case 'error':
+                const err: string = response.value as string;
+                // TODO: Handle error
+                break;
+        }
     }
 }
--- webserver/static/isptoolbox/src/MarketEvaluatorWS.ts
+++ webserver/static/isptoolbox/src/MarketEvaluatorWS.ts
@@ -40,80 +40,93 @@ type MedianSpeed = {
     'Upload (Mbit/s)': string,
     Zipcode: string,
     pct_area?: string,
-  };
-
-type MedianSpeedResponse = Array<MedianSpeed>;
-
-type ServiceProvidersResponse = {
-  error: number,
-  competitors: Array<string>,
-  down_ad_speed: Array<number>,
-  tech_used: Array<Array<number>>,
-  up_ad_speed: Array<number>,
 };
 
-type BroadbandNowResponse = {
-  bbnPriceRange: Array<string> | null,
+export type MedianSpeedResponse = Array<MedianSpeed>;
+
+export type ServiceProvidersResponse = {
+    error: number,
+    competitors: Array<string>,
+    down_ad_speed: Array<number>,
+    tech_used: Array<Array<number>>,
+    up_ad_speed: Array<number>,
 };
 
-type BuildingOverlaysResponse = {
-  done: boolean,
-  gc: {
-    type: string,
-    geometries: Array<GeoJSON.Polygon>,
-  },
-  offset: string,
+export type BroadbandNowResponse = {
+    bbnPriceRange: Array<string> | null,
 };
 
-type MedianIncomeResponse = {
-  averageMedianIncome: number,
-  error?: string,
+export type BuildingOverlaysResponse = {
+    done: boolean,
+    gc: {
+        type: string,
+        geometries: Array<GeoJSON.Polygon>,
+    },
+    offset: string,
+};
+
+export type MedianIncomeResponse = {
+    averageMedianIncome: number,
+    error?: string,
 };
 
 type AuthenticationResponse = {
-  token?: string,
-  error?: string,
+    token?: string,
+    error?: string,
 };
 
 type MarketEvaluatorWSValue =
-  | MedianSpeedResponse
-  | ServiceProvidersResponse
-  | BroadbandNowResponse
-  | BuildingOverlaysResponse
-  | MedianIncomeResponse
-  | RDOFGeojsonResponse
-  | ZipGeojsonResponse
-  | CountyGeojsonResponse
-  | ViewshedGeojsonResponse
-  | AuthenticationResponse
-  | string;
+    | MedianSpeedResponse
+    | ServiceProvidersResponse
+    | BroadbandNowResponse
+    | BuildingOverlaysResponse
+    | MedianIncomeResponse
+    | RDOFGeojsonResponse
+    | ZipGeojsonResponse
+    | CountyGeojsonResponse
+    | ViewshedGeojsonResponse
+    | AuthenticationResponse
+    | string;
 
-type MarketEvaluatorWSResponse = {
-  type: string,
-  uuid: string,
-  value: MarketEvaluatorWSValue,
+export type MarketEvaluatorWSResponse = {
+    type: string,
+    uuid: string,
+    value: MarketEvaluatorWSValue,
 };
 
 
 interface MarketEvaluatorWSCallback {
-    (message : MarketEvaluatorWSResponse) : void
+    (message: MarketEvaluatorWSResponse): void;
+}
+
+export type UUID = string;
+
+/**
+ * UUID function from core WWW
+ * Based on the rfc4122-compliant solution posted at
+ * http://stackoverflow.com/questions/105034
+ */
+export function uuid(): UUID {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+    const r = (Math.random() * 16) | 0;
+    const v = c == 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
 }
 
 class MarketEvaluatorWS {
-    ws : WebSocket;
-    networkName : string;
+    ws: WebSocket;
     message_handlers: Array<MarketEvaluatorWSCallback>;
-    hash : string = '';
-    constructor(networkName : string, message_handlers : Array<MarketEvaluatorWSCallback>){
-        this.networkName = networkName;
+    hash: string = '';
+    constructor(message_handlers: Array<MarketEvaluatorWSCallback>) {
         this.message_handlers = message_handlers;
         this.connect();
     }
 
-    setConnectionStatus(connected: boolean){
+    protected setConnectionStatus(connected: boolean) {
         const element = $('#websocket-connection-status');
         const geocoder = $('#geocoder');
-        if(connected){
+        if (connected) {
             element.addClass('d-none');
             geocoder.removeClass('d-none');
         } else {
@@ -122,29 +135,136 @@ class MarketEvaluatorWS {
         }
     }
 
-    connect(){
+    protected connect() {
         const protocol = location.protocol !== 'https:' ? 'ws://' : 'wss://';
         const domain = location.protocol !== 'https:' ? location.host : 'isptoolbox.io';
-        this.ws = new WebSocket(protocol + domain + '/ws/market-evaluator/' + this.networkName + '/');
+        this.ws = new WebSocket(protocol + domain + '/ws/market-evaluator/');
 
 
         this.ws.onclose = (e) => {
             this.setConnectionStatus(false);
-            setTimeout(()=> {
+            setTimeout(() => {
                 this.connect();
-            }, 1000)
-        }
+            }, 1000);
+        };
 
         this.ws.onopen = (e) => {
             this.setConnectionStatus(true);
-        }
+            // TODO: Do we want authentication on the websocket layer? 
+            // If not, need to remove this code on FE and BE sides the way it currently is setup
+            const authJson = {
+                credentials: 'default',
+            };
+            this.ws.send(JSON.stringify(authJson));
+        };
 
         this.ws.onmessage = (e) => {
             const resp = JSON.parse(e.data) as MarketEvaluatorWSResponse;
-            this.message_handlers.forEach((handler)=>{
+            // Abstract away mock auth for now
+            if (resp.type === 'auth.token') {
+                return;
+            }
+            this.message_handlers.forEach((handler) => {
                 handler(resp);
             });
+        };
+    }
+
+    /**
+     * Sends JSON object on the websocket, attaching an appropriate UUID to the request and returning it.
+     * @param req Json Object
+     * @returns The request-identifying UUID sent with the request
+     */
+    private sendJson(req: Object): UUID {
+        const reqUUID: UUID = uuid();
+        const reqWithUUID = {
+            ...req,
+            uuid: reqUUID,
         }
+        this.ws.send(JSON.stringify(reqWithUUID));
+        return reqUUID;
+    }
+
+    /**
+     * Main request of Market Evaluator, requests evaluation of a given GeoJSON area.
+     * @param include GeoJSON area of interest
+     * @returns The request-identifying UUID
+     */
+    sendPolygonRequest(include: GeoJSON.FeatureCollection): UUID {
+        return this.sendJson({
+            request_type: 'standard_polygon',
+            include,
+        });
+    }
+
+    /**
+     * Requests RDOF GeoJSON corresponsing to given census block group ID.
+     * @param cbgid census block id
+     * @returns The request-identifying UUID
+     */
+    sendRDOFRequest(cbgid: string): UUID {
+        return this.sendJson({
+            request_type: 'grant',
+            cbgid,
+        });
+    }
+
+    /**
+     * Requests Zip GeoJSON corresponsing to given zipcode.
+     * @param zip zipcode
+     * @returns The request-identifying UUID
+     */
+    sendZipRequest(zip: string): UUID {
+        return this.sendJson({
+            request_type: 'zip',
+            zip,
+        });
+    }
+
+    /**
+     * Requests County GeoJSON corresponsing to given county and statecode.
+     * @param countycode the county code
+     * @param statecode the state code
+     * @returns The request-identifying UUID
+     */
+    sendCountyRequest(countycode: string, statecode: string): UUID {
+        return this.sendJson({
+            request_type: 'county',
+            countycode,
+            statecode,
+        });
+    }
+
+    /**
+     * Requests census block GeoJSON corresponsing to given blockcode.
+     * @param blockcode the census block code
+     * @returns The request-identifying UUID
+     */
+    sendCensusBlockRequest(blockcode: string): UUID {
+        return this.sendJson({
+            request_type: 'census_block',
+            blockcode,
+        });
+    }
+
+    /**
+     * Requests CloudRF viewshed associated with given parameters.
+     * @param customerHeight CPE height in meters
+     * @param height Height in meters
+     * @param lat latitude as float
+     * @param lon longitude as float
+     * @param radius Radius in meters
+     * @returns The request-identifying UUID
+     */
+    sendViewshedRequest(customerHeight: number, height: number, lat: number, lon: number, radius: number): UUID {
+        return this.sendJson({
+            request_type: 'viewshed',
+            customerHeight,
+            height,
+            lat,
+            lon,
+            radius,
+        });
     }
 }
 

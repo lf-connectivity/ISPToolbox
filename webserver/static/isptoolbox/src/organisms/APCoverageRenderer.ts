5608090e6bc9579209291a7d5de6b8fb791af315
Kevin Zheng <kevinz@fb.com>
2021-06-28 22:40:58 +0000

    Removed commented out stuff.

--- webserver/static/isptoolbox/src/organisms/APCoverageRenderer.ts
+++ webserver/static/isptoolbox/src/organisms/APCoverageRenderer.ts
@@ -175,11 +175,6 @@ abstract class RadiusAndBuildingCoverageRenderer {
     renderSelectedAccessPoints(msg: string, data: any){
         // If there are selected APs or coverage areas, only render circles/buildings for those, otherwise render all.
         let fc = this.draw.getSelected();
-        // let selectedAPs = this.workspaceManager.filterByType(fc.features, WorkspaceFeatureTypes.AP);
-        // let selectedCoverageAreas = this.workspaceManager.filterByType(fc.features, WorkspaceFeatureTypes.COVERAGE_AREA);
-        // if (selectedAPs.length === 0 && selectedCoverageAreas.length == 0) {
-        //     fc = this.draw.getAll();
-        // }
         this.debouncedRenderAPRadius(fc);
     }
 

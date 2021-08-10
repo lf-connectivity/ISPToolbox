0bc87c4de9b4cdc6bcf9d43fcf81e9595bad4c7d
Kevin Zheng <kevinz@fb.com>
2021-08-10 23:29:43 +0000

    Linting.

--- webserver/mmwave/tasks/link_tasks.py
+++ webserver/mmwave/tasks/link_tasks.py
@@ -224,7 +224,11 @@ def getLiDARProfile(network_id, data, resolution=LidarResolution.LOW.value):
         if r and r['resolution'] >= resolution:
             logging.info('lidar cache hit: resolution %s', r['resolution'])
             resp.update({k: r[k] for k in
+<<<<<<< HEAD
                         ('lidar_profile', 'url', 'bb', 'source', 'tx', 'rx', 'aoi', 'resolution', 'res', 'dist')})
+=======
+                        ('lidar_profile', 'url', 'bb', 'source', 'tx', 'rx', 'aoi', 'resolution')})
+>>>>>>> 44bba7c (Linting.)
 
         else:
             logging.info('lidar cache miss for resolution %s', resolution)

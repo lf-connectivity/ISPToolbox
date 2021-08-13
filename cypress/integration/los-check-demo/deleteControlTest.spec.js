3644fe8e8498ed4948ce327269d1a2211ae0ccfc
jmknoll <j.knoll621@gmail.com>
2021-08-13 14:43:32 -0400

    test

--- cypress/integration/los-check-demo/deleteControlTest.spec.js
+++ cypress/integration/los-check-demo/deleteControlTest.spec.js
@@ -5,13 +5,13 @@ context("Test Delete Control", () => {
     cy.visit("/demo/los-check/");
   });
 
-  it("Delete Link Using the Delete Button, proceed to add a new link by drawing on map", () => {
-    cy.contains("resolution", { timeout: 10000 }).should("exist");
-    cy.get(".collapse-button").click();
-    cy.get(".mapbox-gl-draw_trash").click();
-    cy.get("#drawing_instructions", { timeout: 10000 }).should("be.visible");
-    cy.get("#loading_spinner").should("not.be.visible");
-    cy.get(".radio-card-body").should("not.be.visible");
+  it('Delete Link Using the Delete Button, proceed to add a new link by drawing on map', () => {
+    cy.contains('resolution', {timeout: 10000}).should('exist');
+    cy.get('.collapse-button').click();
+    cy.get('.mapbox-gl-draw_trash').click();
+    cy.get('#drawing_instructions', {timeout: 10000}).should('be.visible');
+    cy.get('#loading_spinner').should('not.be.visible');
+    cy.get('.radio-card-body').should('not.be.visible');
 
     // Let's add a link to the map
     cy.get("#add-link-btn").click();

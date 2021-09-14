1ea9747f3da485fc81ad59c53c1524c253edec22
Kevin Zheng <kevinz@fb.com>
2021-09-14 17:00:40 +0000

    Settings debug

--- webserver/webserver/settings.py
+++ webserver/webserver/settings.py
@@ -14,6 +14,8 @@ import os
 import json
 from .secrets import get_secret
 
+import logging
+logging.error('Hello World')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

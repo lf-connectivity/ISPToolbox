aef092c25caad92aa89aae8c4bb6cf4baf323602
Alex Chong <achong@fb.com>
2021-09-28 18:27:00 +0000

    create view in admin panel that filters on super users

--- webserver/IspToolboxAccounts/admin.py
+++ webserver/IspToolboxAccounts/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
 from IspToolboxAccounts.models import NewUserExperience, User, IspToolboxUserSignUpInfo
 
 
@@ -10,6 +12,20 @@ class IspToolboxUserAdmin(admin.ModelAdmin):
 @admin.register(NewUserExperience)
 class ChangeNuxSettings(admin.ModelAdmin):
     change_list_template = 'IspToolboxAccounts/update_nux_settings.html'
+
+
+class SuperUser(User):
+    class Meta:
+        proxy = True
+        verbose_name = 'Super User'
+        verbose_name_plural = 'Super Users'
+
+
+@admin.register(SuperUser)
+class IspToolboxSuperUserAdmin(admin.ModelAdmin):
+    def get_queryset(self, request: HttpRequest) -> QuerySet:
+        super_query = super().get_queryset(request)
+        return super_query.filter(is_superuser=True)
 
 
 admin.site.register(IspToolboxUserSignUpInfo)

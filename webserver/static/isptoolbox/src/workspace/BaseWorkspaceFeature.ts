835380b11177ea1f40bd545d990da2261f85c37b
Alex Chong <achong@fb.com>
2021-10-15 16:48:04 +0000

    cleanup2

--- webserver/static/isptoolbox/src/workspace/BaseWorkspaceFeature.ts
+++ webserver/static/isptoolbox/src/workspace/BaseWorkspaceFeature.ts
@@ -1,7 +1,7 @@
 import mapboxgl, * as MapboxGL from 'mapbox-gl';
 import MapboxDraw from '@mapbox/mapbox-gl-draw';
-import { Feature, Geometry, Point, LineString, GeoJsonProperties, Polygon } from 'geojson';
-import { WorkspaceEvents, WorkspaceFeatureTypes } from './WorkspaceConstants';
+import { Feature, Geometry, Point } from 'geojson';
+import { WorkspaceFeatureTypes } from './WorkspaceConstants';
 import { getCookie } from '../utils/Cookie';
 import { getSessionID } from '../utils/MapPreferences';
 
@@ -229,57 +229,5 @@ export abstract class WorkspacePointFeature extends BaseWorkspaceFeature {
 
     getFeatureGeometryCoordinates() {
         return this.getFeatureGeometry()?.coordinates;
-    }
-}
-
-/**
- * Abstract class containing point-specific functions for Workspace objects.
- */
-export abstract class WorkspaceLineStringFeature extends BaseWorkspaceFeature {
-    /**
-     * Moves the selected vertex to the specified lat/long coordinates. To be
-     * used by other Workspace feature classes for programatically moving objects
-     * in response to other things moving, not as an extension for the Mapbox API.
-     *
-     * Calls successFollowup if defined, presumably for updating other objects.
-     *
-     * @param index Index of coordinate in LineString object to update
-     * @param newCoords New coordinates (lat/long)
-     * @param successFollowup Function to execute after successfully moving object
-     * @returns A list of other BaseWorkspaceFeature objects affected by this move.
-     */
-    moveVertex(index: number, newCoords: [number, number]) {
-        let feature = this.getFeatureData();
-        if (index >= 0 && index < feature.geometry.coordinates.length) {
-            let newFeature = { ...feature };
-            newFeature.geometry.coordinates[index] = newCoords;
-            this.draw.add(newFeature);
-            this.map.fire('draw.update', { features: [newFeature], action: 'move' });
-        }
-    }
-
-    getFeatureData(): Feature<LineString, any> {
-        return super.getFeatureData() as Feature<LineString, any>;
-    }
-
-    getFeatureGeometry(): LineString {
-        return super.getFeatureGeometry() as LineString;
-    }
-
-    getFeatureGeometryCoordinates() {
-        return this.getFeatureGeometry()?.coordinates;
-    }
-}
-
-/**
- * Abstract class containing polygon-specific functions for Workspace objects.
- */
-export abstract class WorkspacePolygonFeature extends BaseWorkspaceFeature {
-    getFeatureData(): Feature<Polygon, any> {
-        return super.getFeatureData() as Feature<Polygon, any>;
-    }
-
-    getFeatureGeometry(): Polygon {
-        return super.getFeatureGeometry() as Polygon;
     }
 }

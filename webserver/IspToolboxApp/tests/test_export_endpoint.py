659b9e2b742d3ffc3d1ed49d12be97b796609751
Alex Chong <achong@fb.com>
2021-10-15 18:30:02 +0000

    fix test

--- webserver/IspToolboxApp/tests/test_export_endpoint.py
+++ webserver/IspToolboxApp/tests/test_export_endpoint.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+import json
 
 
 class TestKMZExportMarketEvaluator(TestCase):
@@ -7,7 +8,17 @@ class TestKMZExportMarketEvaluator(TestCase):
         Test Exporting Outline of Market Evaluator
         """
         client = Client()
-        payload = {"shapes": True, "buildings": False, "include": {"type": "GeometryCollection", "geometries": [{"coordinates": [
-            [[-122.5084755653811, 37.59777287648443], [-122.50805088997956, 37.582461643770614], [-122.48936517232276, 37.580274067702604], [-122.5084755653811, 37.59777287648443]]], "type":"Polygon"}]}}
-        response = client.post('/market-evaluator/export-np/', data=payload)
+        payload = json.dumps(
+            {
+                "shapes": True,
+                "buildings": False,
+                "include": {"type": "GeometryCollection", "geometries": [
+                    {"coordinates": [
+                        [[-122.5084755653811, 37.59777287648443],
+                         [-122.50805088997956, 37.582461643770614],
+                            [-122.48936517232276, 37.580274067702604],
+                            [-122.5084755653811, 37.59777287648443]]
+                    ], "type":"Polygon"}]}})
+        response = client.post('/market-evaluator/export-np/', data=payload, content_type='text/plain'
+                               )
         self.assertIs(response.status_code, 200)

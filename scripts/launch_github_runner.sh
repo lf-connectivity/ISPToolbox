6c990780dd99a0e13bf34323bb6d862a62a4ba7d
Alex Chong <achong@fb.com>
2021-10-23 00:08:41 +0000

    update launch service

--- scripts/launch_github_runner.sh
+++ scripts/launch_github_runner.sh
@@ -24,6 +24,6 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
-
-sudo ./svc.sh install
+# Install Github Runner Service as Root
+sudo ./svc.sh install root
 sudo ./svc.sh start

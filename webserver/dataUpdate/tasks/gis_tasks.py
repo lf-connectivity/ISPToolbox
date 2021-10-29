40302a710ed023a1e205bca851bda6017547923d
Alex Chong <achong@fb.com>
2021-10-29 23:14:26 +0000

    change to @app.task from shared_task

--- webserver/dataUpdate/tasks/gis_tasks.py
+++ webserver/dataUpdate/tasks/gis_tasks.py
@@ -3,7 +3,6 @@ from dataUpdate.scripts.update_mlab import updateMlab
 from dataUpdate.scripts.update_non_urban import update_community_connect
 from dataUpdate.scripts.update_cbrs import updateCbrs
 from dataUpdate.scripts.update_asr_towers import update_asr_towers
-from celery import shared_task
 
 
 @app.task
@@ -13,21 +12,21 @@ def updateGISData():
     updateCbrs()
 
 
-@shared_task
+@app.task
 def updateMlabData():
     updateMlab()
 
 
-@shared_task
+@app.task
 def updateCCData():
     update_community_connect()
 
 
-@shared_task
+@app.task
 def updateCbrsData():
     updateCbrs()
 
 
-@shared_task
+@app.task
 def updateAsrTowers():
     update_asr_towers()

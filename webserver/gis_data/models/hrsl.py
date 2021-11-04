f1924412616a251578d75b380c790408fe351373
Alex Chong <achong@fb.com>
2021-11-04 19:50:34 +0000

    add brazil hrsl model to django

--- webserver/gis_data/models/hrsl.py
+++ webserver/gis_data/models/hrsl.py
@@ -12,7 +12,6 @@ class HrslUsa15(models.Model):
     https://data.humdata.org/dataset/united-states-high-resolution-population-density-maps-demographic-estimates
     """
     rid = models.AutoField(primary_key=True)
-    # This field type is a guess.
     rast = models.RasterField(blank=True, null=True)
 
     class Meta:
@@ -41,6 +40,48 @@ class HrslUsa15(models.Model):
             SELECT (ST_SummaryStatsAgg(raster_data.intersect, 1, false)).sum FROM raster_data;
         """
         with connections[HrslUsa15.objects.db].cursor() as cursor:
+            cursor.execute(
+                query, [area_of_interest.ewkb, area_of_interest.ewkb])
+            res = cursor.fetchone()
+            return res
+
+
+class HrslBra15(models.Model):
+    """
+    High Resolution Spatial Layer - Facebook Dataset
+
+    Has attribution requirements
+    https://data.humdata.org/dataset/united-states-high-resolution-population-density-maps-demographic-estimates
+    """
+    rid = models.AutoField(primary_key=True)
+    rast = models.RasterField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'hrsl_bra_1_5'
+
+    @staticmethod
+    def get_intersection_population(area_of_interest: GEOSGeometry, read_only: bool):
+        query = f"""
+            WITH raster_data as (
+                SELECT
+                    ST_Clip(
+                        {HrslBra15.rast.field.column},
+                        ST_GeomFromEWKB(%s),
+                        0,
+                        TRUE
+                    ) as intersect
+                FROM
+                    {HrslBra15._meta.db_table}
+                WHERE
+                    ST_Intersects(
+                        {HrslBra15.rast.field.column},
+                        ST_GeomFromEWKB(%s)
+                    )
+            )
+            SELECT (ST_SummaryStatsAgg(raster_data.intersect, 1, false)).sum FROM raster_data;
+        """
+        with connections[HrslBra15.objects.db].cursor() as cursor:
             cursor.execute(
                 query, [area_of_interest.ewkb, area_of_interest.ewkb])
             res = cursor.fetchone()

06167c2fcc87ff19cb08a842772c447b57c3a7e7
Kevin Zheng <kevinz@fb.com>
2021-11-10 18:27:12 +0000

    Remove console.log

--- webserver/static/isptoolbox/src/molecules/MapboxGeoOverlay.ts
+++ webserver/static/isptoolbox/src/molecules/MapboxGeoOverlay.ts
@@ -145,7 +145,6 @@ abstract class MapboxGeoOverlay implements MapboxOverlay {
                 id: ''
             };
 
-            console.log(newFeature);
             // @ts-ignore
             let id = this.draw.add(newFeature)[0];
             newFeature.id = id;

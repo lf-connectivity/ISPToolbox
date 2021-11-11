89d700abcd5515cb9e974396e67717fca1f9727a
Alex Chong <achong@fb.com>
2021-11-11 19:14:44 +0000

    add quick e2e test for exporting sessions

---
+++ cypress/integration/workspace/export-session.spec.js
@@ -0,0 +1,44 @@
+/// <reference types="cypress" />
+
+describe("Test Exporting Workspace Session", () => {
+    before(() => { });
+
+    beforeEach(() => {
+        cy.login();
+    });
+
+    it("Verify that geojson and kml links are present and work", function () {
+        cy.visit("/pro/network/edit/");
+        cy.close_disclaimer();
+        cy.close_nux();
+        cy.contains('Editing').click();
+        cy.contains('Export').click();
+
+        // Assert that session kml file is downloaded
+        const kml_url = cy.get('a').contains('KML').invoke('attr', 'href').then(href => {
+            cy.log(href);
+            cy.request({
+                url: href,
+                encoding: 'base64'
+            })
+                .then((response) => {
+                    // 3️⃣
+                    expect(response.status).to.equal(200);
+                });
+        });
+
+
+        // Assert that session geojson file is downloaded
+        const geojson_url = cy.get('a').contains('GeoJSON').invoke('attr', 'href').then(href => {
+            cy.log(href);
+            cy.request({
+                url: href,
+                encoding: 'base64'
+            })
+                .then((response) => {
+                    // 3️⃣
+                    expect(response.status).to.equal(200);
+                });
+        });
+    });
+});

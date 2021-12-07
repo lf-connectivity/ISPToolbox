8bc2fbed8e76e16f9882258ce6daf163729231a3
alexychongfb <57922324+alexychongfb@users.noreply.github.com>
2021-12-07 10:26:15 -0800

    update e2e tests

--- cypress/integration/workspace/workspace-offline-crud.spec.js
+++ cypress/integration/workspace/workspace-offline-crud.spec.js
@@ -12,40 +12,44 @@ context('Work', () => {
     it('Test Disconnection Trying To Create Workspace Features', () => {
     	// Network App
         cy.visit('/pro/network/edit/');
+        cy.close_disclaimer();
         cy.close_nux();
         cy.wait_mapbox();
         cy.contains('Place Tower').click();
         cy.window().then((win) => {
-            expect(win.IspToolboxApp.draw.getMode()).to.equal('draw_ap');
+            expect(win.mapbox_handles.draw.getMode()).to.equal('draw_ap');
 		})
 		cy.wait(500);
 
         cy.goOffline();
         cy.get('#connection_issues_alert').should('be.visible'); 
         cy.get('#map').should('be.visible').click(400, 400);
+        cy.get('#ajax-failed_alert').should('be.visible'); 
         cy.goOnline();
 
         cy.window().then((win) => {
-        	expect(win.IspToolboxApp.draw.getAll().features.length).to.equal(0);
+        	expect(win.mapbox_handles.draw.getAll().features.length).to.equal(3);
 		})
 
 		// Market Evaluator
 		cy.visit('/pro/market/');
+        cy.close_disclaimer();
         cy.close_nux();
         cy.wait_mapbox();
         cy.contains('Place Tower').click();
         cy.window().then((win) => {
-            expect(win.IspToolboxApp.draw.getMode()).to.equal('draw_ap');
+            expect(win.mapbox_handles.draw.getMode()).to.equal('draw_ap');
 		})
 		cy.wait(500);
 
         cy.goOffline();
         cy.get('#connection_issues_alert').should('be.visible'); 
         cy.get('#map').should('be.visible').click(400, 400);
+        cy.get('#ajax-failed_alert').should('be.visible'); 
         cy.goOnline();
 
         cy.window().then((win) => {
-        	expect(win.IspToolboxApp.draw.getAll().features.length).to.equal(0);
+        	expect(win.mapbox_handles.draw.getAll().features.length).to.equal(3);
 		})
     });
 })

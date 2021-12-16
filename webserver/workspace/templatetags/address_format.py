f11f947cdaa7efef206867f3d492a88e4b62e989
Kevin Zheng <kevinz@fb.com>
2021-12-16 19:26:48 +0000

    linting

--- webserver/workspace/templatetags/address_format.py
+++ webserver/workspace/templatetags/address_format.py
@@ -17,41 +17,43 @@ _geocoder = Geocoder(access_token=settings.MAPBOX_ACCESS_TOKEN_PUBLIC)
 
 
 _MAPBOX_TO_ADDRESS_FORMATTER_COMPONENTS = {
-    'country': 'country',
-    'region': 'state',
-    'postcode': 'postcode',
-    'district': 'state_district',
-    'place': 'city',
-    'locality': 'city_district',
-    'address': 'road'
+    "country": "country",
+    "region": "state",
+    "postcode": "postcode",
+    "district": "state_district",
+    "place": "city",
+    "locality": "city_district",
+    "address": "road",
 }
+
 
 def _process_component(response_component):
     address_components = {}
-    if 'id' in response_component:
-        mapbox_component_type = response_component['id'].split('.')[0]
+    if "id" in response_component:
+        mapbox_component_type = response_component["id"].split(".")[0]
 
         # ignore neighborhood data type because it lacks official status
         if mapbox_component_type in _MAPBOX_TO_ADDRESS_FORMATTER_COMPONENTS:
             component_type = _MAPBOX_TO_ADDRESS_FORMATTER_COMPONENTS[mapbox_component_type]
-            address_components[component_type] = response_component['text']
+            address_components[component_type] = response_component["text"]
 
             # Addresses might have house number
-            if component_type == 'road' and 'address' in response_component:
-                address_components['house_number'] = response_component['address']
+            if component_type == "road" and "address" in response_component:
+                address_components["house_number"] = response_component["address"]
 
     return address_components
+
 
 def _reverse_geocode(lat, long):
     address_components = {}
     response = _geocoder.reverse(lon=long, lat=lat)
     if response.status_code == HTTP_200_OK:
-        best_fit = response.geojson()['features'][0]
+        best_fit = response.geojson()["features"][0]
         address_components.update(_process_component(best_fit))
 
         # Process response into address components
-        if 'context' in best_fit:
-            for item in best_fit['context']:
+        if "context" in best_fit:
+            for item in best_fit["context"]:
                 address_components.update(_process_component(item))
 
         return address_components
@@ -62,24 +64,26 @@ def _reverse_geocode(lat, long):
 class _UTF8AddressFormatter(international_address_formatter.AddressFormatter):
     def __init__(self):
         file_dir = os.path.dirname(os.path.abspath(international_address_formatter.__file__))
-        config = os.path.abspath(os.path.join(file_dir, 'data/worldwide.yml'))
+        config = os.path.abspath(os.path.join(file_dir, "data/worldwide.yml"))
 
-        with open(config, 'r', encoding='latin-1') as f:
-            contents = f.read().encode('utf-8')
+        with open(config, "r", encoding="latin-1") as f:
+            contents = f.read().encode("utf-8")
 
             # Exceptionally dangerous but we trust this file input so it should be ok :)
             self.model = yaml.load(contents, Loader=yaml.FullLoader)
-    
+
     def _get_country_format(self, country_code):
-        search_key = country_code.upper() if country_code is not None else 'default'
+        search_key = country_code.upper() if country_code is not None else "default"
         fmt = self.model.get(search_key, None)
         if fmt is None:
-            fmt = self.model.get('default', None)
+            fmt = self.model.get("default", None)
         if fmt is None:
-            raise RuntimeError("Configuration file for address formatter has no default value!")
-        
+            raise RuntimeError(
+                "Configuration file for address formatter has no default value!"
+            )
+
         return fmt
-    
+
     def format(self, address, country_code=None):
         fmt = self._get_country_format(country_code)
 
@@ -87,20 +91,22 @@ class _UTF8AddressFormatter(international_address_formatter.AddressFormatter):
         for key, value in address.items():
             if value is not None:
                 cleaned_address[key] = value
-        
+
         # Add support for use_country, change_country, and add_component
-        if 'change_country' in fmt:
-            cleaned_address['country'] = fmt['change_country']
-        if 'add_component' in fmt:
-            k, v = fmt['add_component'].split('=', 2)
+        if "change_country" in fmt:
+            cleaned_address["country"] = fmt["change_country"]
+        if "add_component" in fmt:
+            k, v = fmt["add_component"].split("=", 2)
             cleaned_address[k] = v
-        if 'use_country' in fmt:
-            fmt = self._get_country_format(fmt['use_country'])
-        
+        if "use_country" in fmt:
+            fmt = self._get_country_format(fmt["use_country"])
+
         # TODO: add support for postformat_replace (need for BR)
-        
-        cleaned_address['first'] = international_address_formatter.format.first(cleaned_address)
-        return pystache.render(fmt['address_template'], cleaned_address).strip() 
+
+        cleaned_address["first"] = international_address_formatter.format.first(
+            cleaned_address
+        )
+        return pystache.render(fmt["address_template"], cleaned_address).strip()
 
 
 @register.simple_tag
@@ -109,9 +115,9 @@ def reverse_geocoded_address_lines(lat, long, include_country=False):
     Provides a list of the lines forming the reverse geocoded lat long's address.
     """
     address_components = _reverse_geocode(lat, long)
-    country_code = pycountry.countries.get(name=address_components['country']).alpha_2
+    country_code = pycountry.countries.get(name=address_components["country"]).alpha_2
     address_formatter = _UTF8AddressFormatter()
-    lines = address_formatter.format(address_components, country_code=country_code).split('\n')
+    lines = address_formatter.format(address_components, country_code=country_code).split("\n")
     if not include_country:
         lines = lines[:-1]
     return lines

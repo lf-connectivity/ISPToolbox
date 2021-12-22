99b8fd8849451508def7fceeab7b5249ddd3b9db
Kevin Zheng <kevinz@fb.com>
2021-12-22 16:33:35 +0000

    Rewrote frequency dropdown into templates, not tags

--- webserver/workspace/templatetags/isp_toolbox_units.py
+++ webserver/workspace/templatetags/isp_toolbox_units.py
@@ -6,14 +6,6 @@ from workspace.models.model_constants import ModelLimits
 
 register = template.Library()
 
-_HTML_OPTION = """
-    <option value="{}">{} GHz</option>
-"""
-
-_HTML_OPTION_SELECTED = """
-    <option value="{}" selected>{} GHz</option>
-"""
-
 
 # Done here so we don't have to do this in templates 100000 times
 @register.simple_tag
@@ -22,14 +14,6 @@ def unit_preference_value(unit, imperial_value, metric_value):
         return imperial_value
     else:
         return metric_value
-
-
-@register.simple_tag
-def frequency_dropdown_option(frequency, option_value, option_text):
-    if math.isclose(frequency, option_value):
-        return format_html(_HTML_OPTION_SELECTED, option_value, option_text)
-    else:
-        return format_html(_HTML_OPTION, option_value, option_text)
 
 
 @register.simple_tag

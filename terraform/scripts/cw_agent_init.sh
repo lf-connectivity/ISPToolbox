79e1970651cdcb43fd65f011e0c38b46de0f69e1
Alex Chong <achong@fb.com>
2022-01-05 01:53:42 +0000

    add aggreagation by autoscaling group to cw agent config

--- terraform/scripts/cw_agent_init.sh
+++ terraform/scripts/cw_agent_init.sh
@@ -1,2 +1,30 @@
 sudo yum -y install amazon-cloudwatch-agent
-sudo amazon-cloudwatch-agent-ctl -a start
+sudo touch /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json 
+echo '{
+        "agent": {
+                "metrics_collection_interval": 60,
+                "run_as_user": "root"
+        },
+        "metrics": {
+                "namespace": "ISPToolbox/AsyncWorkerDiskStats",
+                "append_dimensions": {
+                        "AutoScalingGroupName": "${aws:AutoScalingGroupName}",
+                        "ImageId": "${aws:ImageId}",
+                        "InstanceId": "${aws:InstanceId}",
+                        "InstanceType": "${aws:InstanceType}"
+                },
+                "aggregation_dimensions" : [["AutoScalingGroupName"], ["InstanceId", "InstanceType"]],
+                "metrics_collected": {
+                        "disk": {
+                                "measurement": [
+                                        "used_percent"
+                                ],
+                                "metrics_collection_interval": 60,
+                                "resources": [
+                                        "*"
+                                ]
+                        }
+                }
+        }
+}' | sudo tee /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -s -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json

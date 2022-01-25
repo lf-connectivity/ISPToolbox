4d1a88df47ac31d72a5f956af5c67b9d4dfd8b5a
Kevin Zheng <kevinz@fb.com>
2022-01-25 23:20:53 +0000

    Market Eval draw selection change callback

--- webserver/static/isptoolbox/src/organisms/MarketEvaluatorRadiusAndBuildingCoverageRenderer.ts
+++ webserver/static/isptoolbox/src/organisms/MarketEvaluatorRadiusAndBuildingCoverageRenderer.ts
@@ -1,15 +1,13 @@
 import mapboxgl from 'mapbox-gl';
 import * as _ from 'lodash';
 import { createGeoJSONCircle } from '../isptoolbox-mapbox-draw/DrawModeUtils';
-import { Geometry} from 'geojson';
+import { Geometry } from 'geojson';
 import {
     SQM_2_SQFT,
     WorkspaceEvents,
     WorkspaceFeatureTypes
 } from '../workspace/WorkspaceConstants';
-import {
-    MarketEvaluatorTowerPopup
-} from '../isptoolbox-mapbox-draw/popups/TowerPopups';
+import { MarketEvaluatorTowerPopup } from '../isptoolbox-mapbox-draw/popups/TowerPopups';
 import { MarketEvaluatorWorkspaceManager } from '../workspace/MarketEvaluatorWorkspaceManager';
 import MarketEvaluatorWS, {
     BuildingOverlaysResponse,
@@ -19,11 +17,15 @@ import MarketEvaluatorWS, {
 import { GeometryCollection } from '@turf/helpers';
 //@ts-ignore
 import geojsonArea from '@mapbox/geojson-area';
+import { MarketEvaluatorSectorPopup } from '../isptoolbox-mapbox-draw/popups/AjaxSectorPopups';
 import {
-    MarketEvaluatorSectorPopup
-} from '../isptoolbox-mapbox-draw/popups/AjaxSectorPopups';
-import { BUILDING_DATA_SOURCE, BUILDING_LAYER, RadiusAndBuildingCoverageRenderer } from './APCoverageRenderer';
-
+    BUILDING_DATA_SOURCE,
+    BUILDING_LAYER,
+    RadiusAndBuildingCoverageRenderer
+} from './APCoverageRenderer';
+import { BaseWorkspaceManager } from '../workspace/BaseWorkspaceManager';
+import { AccessPoint } from '../workspace/WorkspaceFeatures';
+import { AccessPointSector } from '../workspace/WorkspaceSectorFeature';
 
 export class MarketEvaluatorRadiusAndBuildingCoverageRenderer extends RadiusAndBuildingCoverageRenderer {
     buildingOverlays: GeometryCollection;
@@ -73,10 +75,50 @@ export class MarketEvaluatorRadiusAndBuildingCoverageRenderer extends RadiusAndB
         super.drawDeleteCallback({ features });
     }
 
+    drawSelectionChangeCallback({ features }: { features: Array<any> }) {
+        let finalFeatures = features;
+        // Add tower sectors to selection if a tower is selected
+        if (this.draw.getMode() === 'simple_select') {
+            let feats = features.filter(this.shouldRenderFeature);
+            let featIds = new Set(feats.map((f) => f.id));
+            let complete: boolean = true;
+            feats.forEach((f: GeoJSON.Feature) => {
+                if (
+                    f.properties &&
+                    f.properties.feature_type === WorkspaceFeatureTypes.AP &&
+                    this.shouldRenderFeature(f)
+                ) {
+                    let ap = BaseWorkspaceManager.getFeatureByUuid(
+                        f.properties.uuid
+                    ) as AccessPoint;
+                    ap.sectors.forEach((sector: AccessPointSector) => {
+                        if (!featIds.has(sector.mapboxId)) {
+                            complete = false;
+                            featIds.add(sector.mapboxId);
+                        }
+                    });
+                }
+            });
+
+            if (!complete) {
+                // I guess delaying the
+                setTimeout(() => {
+                    this.draw.changeMode('simple_select', {
+                        featureIds: Array.from(featIds)
+                    });
+                }, 10);
+
+                finalFeatures = Array.from(featIds).map((id) => this.draw.get(id));
+            }
+        }
+
+        super.drawSelectionChangeCallback({ features: finalFeatures });
+    }
+
     sendCoverageRequest({ features }: any) {
         let geometries: Geometry[] = [];
 
-        features = this.draw.getSelected().features.filter(this.shouldRenderFeature);
+        features = features.filter(this.shouldRenderFeature);
 
         let featuresToProcess;
         if (features.length === 0) {
@@ -90,22 +132,12 @@ export class MarketEvaluatorRadiusAndBuildingCoverageRenderer extends RadiusAndB
         featuresToProcess.forEach((f: GeoJSON.Feature) => {
             if (f.properties && f.properties.feature_type && this.shouldRenderFeature(f)) {
                 switch (f.properties.feature_type) {
-                    case WorkspaceFeatureTypes.AP:
-                        if (this.cloudRFExists(f)) {
-                            const geometryCollection = JSON.parse(
-                                f.properties?.cloudrf_coverage_geojson_json
-                            );
-                            geometries.push(...geometryCollection.geometries);
-                        } else {
-                            const new_feat = createGeoJSONCircle(
-                                f.geometry,
-                                f.properties.radius ?? 0.1,
-                                f.id
-                            );
-                            geometries.push(new_feat.geometry);
-                        }
                     case WorkspaceFeatureTypes.COVERAGE_AREA:
                         geometries.push(f.geometry);
+                        break;
+                    case WorkspaceFeatureTypes.SECTOR:
+                        geometries.push(this.processSectorFeature(f));
+                        break;
                 }
             }
         });
@@ -154,5 +186,13 @@ export class MarketEvaluatorRadiusAndBuildingCoverageRenderer extends RadiusAndB
             return geojsonArea.geometry(g);
         });
         return [SQM_2_SQFT * Math.min(...areas), SQM_2_SQFT * Math.max(...areas)];
+    }
+
+    private processSectorFeature(f: GeoJSON.Feature) {
+        if (this.cloudRFExists(f)) {
+            return JSON.parse(f.properties?.cloudrf_coverage_geojson_json);
+        } else {
+            return JSON.parse(f.properties?.geojson_json);
+        }
     }
 }

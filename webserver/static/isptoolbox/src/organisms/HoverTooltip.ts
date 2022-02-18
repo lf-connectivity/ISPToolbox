32f50701a779291c54af1b34bd4e4c3343fb6e35
mairaahmadIB <97994272+mairaahmadIB@users.noreply.github.com>
2022-02-18 14:21:25 -0800

    Style/Shepard, Bootstrap Tootips, and Overlay Bar (#1171)
    * add fixed width for layers on both tools
    * Override bootstrap tooltip font
    * tooltip color changes, shepard colors, and dismissing tooltip on hover

--- webserver/static/isptoolbox/src/organisms/HoverTooltip.ts
+++ webserver/static/isptoolbox/src/organisms/HoverTooltip.ts
@@ -2,7 +2,8 @@ export function addHoverTooltip(selector: string, placement: string = 'top') {
     // @ts-ignore
     $(selector).tooltip({
         delay: { show: 500, hide: 100 },
-        placement: placement
+        placement: placement,
+        trigger : 'hover'
     });
 }
 

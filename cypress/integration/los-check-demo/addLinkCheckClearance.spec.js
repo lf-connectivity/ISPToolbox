7586c4bfe2557b83acf8b73946875d3c656e496d
alexychongfb <57922324+alexychongfb@users.noreply.github.com>
2022-02-24 14:21:22 -0800

    fix add link clearance test

--- cypress/integration/los-check-demo/addLinkCheckClearance.spec.js
+++ cypress/integration/los-check-demo/addLinkCheckClearance.spec.js
@@ -15,13 +15,17 @@ context('Test E2E LOS Check', () => {
       // Clicking on the legend should display a tooltip that shows the data sources
       cy.contains('Legend').click();
       cy.get('.isptoolbox-tooltip').should('contain', 'Google Elevation API');
-    });
 
-    it('Manually type radio locations and heights', () => {
-      cy.get('#hgt-0').clear().type('60');
-      cy.get('#hgt-1').clear().type('57');
-      cy.get('#lat-lng-0').clear().type('38.53362, -121.75712');
-      cy.get('#lat-lng-1').clear().type('38.53271, -121.75164{enter}');
+      cy.get('#hgt-0').clear().type('60').blur();
+      cy.wait(500); // input has debounce on change 
+      cy.get('#hgt-1').clear().type('57').blur();
+      cy.wait(500); // input has debounce on change 
+      cy.get('#lat-lng-0').clear().should('be.empty').type('38.53362, -121.75712').blur();
+      cy.wait(500); // input has debounce on change 
+      cy.get('#lat-lng-0').should('have.value', '38.53362, -121.75712');
+      cy.get('#lat-lng-1').clear().should('be.empty').type('38.53271, -121.75164{enter}').blur();
+      cy.wait(500); // input has debounce on change 
+      cy.get('#lat-lng-1').should('have.value', '38.53271, -121.75164');
       cy.get('#link_chart', {timeout: 10000}).should('be.visible').should('contain', 'resolution');
       cy.get('.link-status', {timeout: 60000}).should('contain', 'Link Status').should('contain', 'Failed');
       cy.get('#freq-dropdown').click();

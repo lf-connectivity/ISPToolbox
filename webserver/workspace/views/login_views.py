8c4e1ec071f201d05e98e7a32f6b1b6bf888304a
Kevin Zheng <kevinz@fb.com>
2022-04-19 16:42:46 +0000

    Redirect to optional_info if optional info doesn't exist in update form view

--- webserver/workspace/views/login_views.py
+++ webserver/workspace/views/login_views.py
@@ -5,53 +5,58 @@ from django.views import View
 from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from IspToolboxAccounts.forms import (
-    IspToolboxUserCreationForm, IspToolboxUserSignUpInfoForm, IspToolboxUserAuthenticationForm,
-    IspToolboxUserPasswordChangeForm, IspToolboxUserInfoChangeForm, IspToolboxUserDeleteAccountForm
+    IspToolboxUserCreationForm,
+    IspToolboxUserSignUpInfoForm,
+    IspToolboxUserAuthenticationForm,
+    IspToolboxUserPasswordChangeForm,
+    IspToolboxUserInfoChangeForm,
+    IspToolboxUserDeleteAccountForm,
 )
 from django.contrib.auth import logout
 
 
 class WorkspaceDashboard(LoginRequiredMixin, View):
     def get(self, request):
-        return render(request, 'workspace/pages/dashboard.html')
+        return render(request, "workspace/pages/dashboard.html")
 
 
 class DefaultWorkspaceView(View):
     def get(self, request, **kwargs):
         if request.user.is_authenticated:
-            return redirect(reverse_lazy('workspace:workspace_dashboard'))
-        showSurvey = (not request.user.is_anonymous) and not IspToolboxUserSignUpInfoForm.Meta.model.objects.filter(
+            return redirect(reverse_lazy("workspace:workspace_dashboard"))
+        showSurvey = (
+            not request.user.is_anonymous
+        ) and not IspToolboxUserSignUpInfoForm.Meta.model.objects.filter(
             owner=request.user
         ).exists()
         return render(
             request,
-            'workspace/pages/login_view.html',
+            "workspace/pages/login_view.html",
             {
-                'showSurvey': showSurvey,
-                'showSignUp': True,
-                'showEmailSignUp': False,
-                'authentication_form': IspToolboxUserAuthenticationForm,
-                'sign_up_form': IspToolboxUserCreationForm,
-            }
+                "showSurvey": showSurvey,
+                "showSignUp": True,
+                "showEmailSignUp": False,
+                "authentication_form": IspToolboxUserAuthenticationForm,
+                "sign_up_form": IspToolboxUserCreationForm,
+            },
         )
 
 
 class OptionalInfoWorkspaceView(LoginRequiredMixin, CreateView):
     form_class = IspToolboxUserSignUpInfoForm
-    template_name = 'workspace/pages/optional_info.html'
-    success_url = reverse_lazy('workspace:workspace_dashboard')
+    template_name = "workspace/pages/optional_info.html"
+    success_url = reverse_lazy("workspace:workspace_dashboard")
 
     def form_valid(self, form):
         form.instance.owner = self.request.user
         # Save the Form
         original_response = super().form_valid(form)
         # To complete the sign-up flow -> send user to the page they were finally trying to get to
-        next_url = self.request.POST.get(
-            'next', self.request.GET.get('next', None))
+        next_url = self.request.POST.get("next", self.request.GET.get("next", None))
         url_is_safe = url_has_allowed_host_and_scheme(
             url=next_url,
             allowed_hosts=self.request.get_host(),
-            require_https=self.request.is_secure()
+            require_https=self.request.is_secure(),
         )
         if next_url is not None and url_is_safe:
             return redirect(next_url)
@@ -59,89 +64,115 @@ class OptionalInfoWorkspaceView(LoginRequiredMixin, CreateView):
             return original_response
 
     def get(self, request):
-        if IspToolboxUserSignUpInfoForm.Meta.model.objects.filter(owner=request.user).exists():
-            return redirect('workspace:optional_info_update')
+        if IspToolboxUserSignUpInfoForm.Meta.model.objects.filter(
+            owner=request.user
+        ).exists():
+            return redirect("workspace:optional_info_update")
         else:
             return super().get(request)
 
 
 class OptionalInfoWorkspaceUpdateView(LoginRequiredMixin, UpdateView):
     form_class = IspToolboxUserSignUpInfoForm
-    template_name = 'workspace/pages/optional_info.html'
-    success_url = reverse_lazy('workspace:workspace_dashboard')
+    template_name = "workspace/pages/optional_info.html"
+    success_url = reverse_lazy("workspace:workspace_dashboard")
 
     def get_object(self):
-        return IspToolboxUserSignUpInfoForm.Meta.model.objects.get(owner=self.request.user)
+        return IspToolboxUserSignUpInfoForm.Meta.model.objects.get(
+            owner=self.request.user
+        )
+
+    def get(self, request):
+        if IspToolboxUserSignUpInfoForm.Meta.model.objects.filter(
+            owner=request.user
+        ).exists():
+            return super().get(request)
+        else:
+            return redirect("workspace:optional_info")
 
 
 class AccountSettingsView(LoginRequiredMixin, View):
     def get(self, request, **kwargs):
         context = {
-            'nav_include_account_dropdown': True,
-            'account_form': IspToolboxUserInfoChangeForm(instance=request.user),
-            'password_form': IspToolboxUserPasswordChangeForm(user=request.user),
-            'delete_account_form': IspToolboxUserDeleteAccountForm,
-            'optional_account_form':
-                IspToolboxUserSignUpInfoForm(
-                    instance=IspToolboxUserSignUpInfoForm.Meta.model.objects.get(
-                        owner=request.user)
-                    if IspToolboxUserSignUpInfoForm.Meta.model.objects.filter(owner=request.user)
-                    else None
+            "nav_include_account_dropdown": True,
+            "account_form": IspToolboxUserInfoChangeForm(instance=request.user),
+            "password_form": IspToolboxUserPasswordChangeForm(user=request.user),
+            "delete_account_form": IspToolboxUserDeleteAccountForm,
+            "optional_account_form": IspToolboxUserSignUpInfoForm(
+                instance=IspToolboxUserSignUpInfoForm.Meta.model.objects.get(
+                    owner=request.user
+                )
+                if IspToolboxUserSignUpInfoForm.Meta.model.objects.filter(
+                    owner=request.user
+                )
+                else None
             ),
         }
         return render(
-            request,
-            'workspace/pages/account_settings_page.html',
-            context=context
+            request, "workspace/pages/account_settings_page.html", context=context
         )
 
     def post(self, request, **kwargs):
         context = {
-            'nav_include_account_dropdown': True,
-            'account_form': IspToolboxUserInfoChangeForm(request.POST, instance=request.user),
-            'password_form': IspToolboxUserPasswordChangeForm(request.user, request.POST),
-            'delete_account_form': IspToolboxUserDeleteAccountForm(request.POST),
-            'optional_account_form': IspToolboxUserSignUpInfoForm(
-                request.POST, instance=(
+            "nav_include_account_dropdown": True,
+            "account_form": IspToolboxUserInfoChangeForm(
+                request.POST, instance=request.user
+            ),
+            "password_form": IspToolboxUserPasswordChangeForm(
+                request.user, request.POST
+            ),
+            "delete_account_form": IspToolboxUserDeleteAccountForm(request.POST),
+            "optional_account_form": IspToolboxUserSignUpInfoForm(
+                request.POST,
+                instance=(
                     IspToolboxUserSignUpInfoForm.Meta.model.objects.get(
-                        owner=request.user)
-                    if IspToolboxUserSignUpInfoForm.Meta.model.objects.filter(owner=request.user).exists()
+                        owner=request.user
+                    )
+                    if IspToolboxUserSignUpInfoForm.Meta.model.objects.filter(
+                        owner=request.user
+                    ).exists()
                     else None
-                )
-            )
+                ),
+            ),
         }
         if "update_account" in request.POST:
-            if context['account_form'].is_valid():
-                context['account_form'].save()
+            if context["account_form"].is_valid():
+                context["account_form"].save()
         else:
             context.update(
-                {'account_form': IspToolboxUserInfoChangeForm(instance=request.user)})
+                {"account_form": IspToolboxUserInfoChangeForm(instance=request.user)}
+            )
 
         if "change_password" in request.POST:
-            if context['password_form'].is_valid():
-                context['password_form'].save()
+            if context["password_form"].is_valid():
+                context["password_form"].save()
         else:
             context.update(
-                {'password_form': IspToolboxUserPasswordChangeForm(user=request.user)})
+                {"password_form": IspToolboxUserPasswordChangeForm(user=request.user)}
+            )
 
         if "user_info" in request.POST:
-            if context['optional_account_form'].is_valid():
-                context['optional_account_form'].instance.owner = request.user
-                context['optional_account_form'].save()
-                context['form_save_success'] = True
+            if context["optional_account_form"].is_valid():
+                context["optional_account_form"].instance.owner = request.user
+                context["optional_account_form"].save()
+                context["form_save_success"] = True
         else:
-            context.update({'optional_account_form': IspToolboxUserSignUpInfoForm(
-                instance=IspToolboxUserSignUpInfoForm.Meta.model.objects.get(
-                    owner=request.user)
-            )})
+            context.update(
+                {
+                    "optional_account_form": IspToolboxUserSignUpInfoForm(
+                        instance=IspToolboxUserSignUpInfoForm.Meta.model.objects.get(
+                            owner=request.user
+                        )
+                    )
+                }
+            )
 
-        if "delete_account" in request.POST and context['delete_account_form'].is_valid():
-            if context['delete_account_form'].try_delete(request):
+        if (
+            "delete_account" in request.POST
+            and context["delete_account_form"].is_valid()
+        ):
+            if context["delete_account_form"].try_delete(request):
                 logout(request)
-                return redirect('workspace:workspace_dashboard')
+                return redirect("workspace:workspace_dashboard")
 
-        return render(
-            request,
-            'workspace/pages/account_settings_page.html',
-            context
-        )
+        return render(request, "workspace/pages/account_settings_page.html", context)

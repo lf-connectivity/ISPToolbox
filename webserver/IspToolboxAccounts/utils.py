3f2dae288306efc10e9241b1532566e779685cb8
Kevin Zheng <kevinz@fb.com>
2022-04-26 15:51:40 +0000

    remove funny import

--- webserver/IspToolboxAccounts/utils.py
+++ webserver/IspToolboxAccounts/utils.py
@@ -1,9 +1,8 @@
-from xmlrpc.client import Boolean
 import waffle
 
 
 ACCOUNT_CREATION_SWITCH = "ENABLE_ACCOUNT_CREATION"
 
 
-def enable_account_creation() -> Boolean:
+def enable_account_creation():
     return waffle.switch_is_active(ACCOUNT_CREATION_SWITCH)
